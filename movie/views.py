ab69b141cc908c9c2c2ddee6237b7366faacd444
Sara Zuluaga <sarazuluagatrujillo@gmail.com>
2024-08-01 10:51:48 -0500

    cambio de color del nombre

--- movie/views.py
+++ movie/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 
 def home(request):
     #return HttpResponse('<h1>Welcome to home page </h1>')
-    return render(request, 'home.html', {'name':'Sara'})
+    return render(request, 'home.html', {'name':'Sara Zuluaga'})
+
+def about(request):
+    return render(request,'about.html')
