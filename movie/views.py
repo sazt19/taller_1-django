4c02222a3033d4e64bc03aa8549bc1cd7550f83c
Sara Zuluaga <sarazuluagatrujillo@gmail.com>
2024-08-26 10:56:04 -0500

    mailing list

--- movie/views.py
+++ movie/views.py
@@ -22,10 +22,15 @@ def home(request):
 def about(request):
     return render(request,'about.html')
 
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email':email})
+
 def statistics_view(request):
     matplotlib.use('Agg')
     years = Movie.objects.values_list('year', flat=True).distinct().order_by('year')
     genres = Movie.objects.values_list('genre', flat=True).distinct().order_by('genre')
+    movies = Movie.objects.all()
     movie_counts_by_year = {}
     movie_counts_by_genre = {}
     for year in years:
@@ -37,17 +42,16 @@ def statistics_view(request):
         count = movie_in_year.count()
         movie_counts_by_year[year] = count
 
-    for genre in genres:
-        if genres:
-            movie_in_genre = Movie.objects.filter(genre=genre)
+    for movie in movies:
+        genre_value = movie.genre
+        first_genre = genre_value.split(',')[0].strip() if genre_value else "Unknown"
+
+        if first_genre in movie_counts_by_genre:
+            movie_counts_by_genre[first_genre] += 1
         else:
-            movie_in_genre = Movie.objects.filter(genre__isnull=True)
-            genre = "None"
-        count_g = movie_in_genre.count()
-        movie_counts_by_genre[genre] = count_g
+            movie_counts_by_genre[first_genre] = 1
 
     bar_width = 0.5
-    bar_spacing = 0.5
     bar_positions = range(len(movie_counts_by_year))
     bar_positions_genre = range(len(movie_counts_by_genre))
 
@@ -64,12 +68,12 @@ def statistics_view(request):
     plt.close()
 
     #movies per genre chart
-    plt.bar(bar_positions_genre, movie_counts_by_genre.values(), width=0.7, align='center')
+    plt.bar(bar_positions_genre, movie_counts_by_genre.values(), width=0.5, align='center')
     plt.title('Movies per genre')
     plt.xlabel('Genre')
     plt.ylabel('Number of movies')
     plt.xticks(bar_positions_genre, movie_counts_by_genre.keys(), rotation=90)
-    plt.subplots_adjust(bottom=0.4)
+    plt.subplots_adjust(bottom=0.3)
     plt.tight_layout()
     buffer_1 = io.BytesIO()
     plt.savefig(buffer_1, format='png')
--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -121,14 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    'moviereviews/static/',
+    'moviereviews/static',
 ]
