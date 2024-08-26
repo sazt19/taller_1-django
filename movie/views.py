c726712043264be74703f5c5cdc4e236e5604210
Sara Zuluaga <sarazuluagatrujillo@gmail.com>
2024-08-25 19:34:35 -0500

    statistics

--- movie/views.py
+++ movie/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Movie
+import matplotlib.pyplot as plt
+import matplotlib
+import io
+import urllib, base64
 
 # Create your views here.
 
@@ -17,3 +21,38 @@ def home(request):
 
 def about(request):
     return render(request,'about.html')
+
+def statistics_view(request):
+    matplotlib.use('Agg')
+    years = Movie.objects.values_list('year', flat=True).distinct().order_by('year')
+    movie_counts_by_year = {}
+    for year in years:
+        if year:
+            movie_in_year = Movie.objects.filter(year=year)
+        else:
+            movie_in_year = Movie.objects.filter(year__insull=True)
+            year = "None"
+        count = movie_in_year.count()
+        movie_counts_by_year[year] = count
+
+    bar_width = 0.5
+    bar_spacing =0.5
+    bar_positions = range(len(movie_counts_by_year))
+
+    plt.bar(bar_positions, movie_counts_by_year.values(), width=bar_width, align='center')
+    plt.title('Movies per year')
+    plt.xlabel('Year')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions, movie_counts_by_year.keys(), rotation=90)
+    plt.subplots_adjust(bottom=0.3)
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+
+    image_png = buffer.getvalue()
+    buffer.close()
+    graphic = base64.b64decode(image_png)
+    graphic = graphic.decode('utf-8')
+
+    return render(request, 'statistics.html', {'graphic':graphic})
